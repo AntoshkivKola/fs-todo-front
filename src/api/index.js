5c9a5ecb5d89f2a5ca05500810b688bf34ff3989
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-04-15 18:48:11 +0300

    Refactor api

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import queryString from 'query-string';
 
 const httpClient = axios.create({
   baseURL: 'http://localhost:3000/api',
@@ -9,14 +8,17 @@ export const createTask = data => {
   const responsePromise = httpClient.post('/tasks', data);
   return responsePromise;
 };
-export const getTasks = data => {
+
+export const getTasks = () => {
   const responsePromise = httpClient.get('/tasks');
   return responsePromise;
 };
+
 export const updateTask = ({ id, values }) => {
   const responsePromise = httpClient.patch(`/tasks/${id}`, values);
   return responsePromise;
 };
+
 export const deleteTask = ({ id }) => {
   const responsePromise = httpClient.delete(`/tasks/${id}`);
   return responsePromise;
