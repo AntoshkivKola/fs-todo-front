5372c550da03ba5edb56d8f4174137f0ebe13ba8
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-04-15 18:46:52 +0300

    Refactor deadline input

--- src/components/TaskForm/index.jsx
+++ src/components/TaskForm/index.jsx
@@ -1,18 +1,27 @@
 import { Formik, Form, Field } from 'formik';
 
-
+import { useState } from 'react';
 import * as TaskActionCreator from '../../actions/taskCreators';
 import { connect } from 'react-redux';
 import styles from './TaskForm.module.scss';
 
-
 const TaskForm = props => {
   const { createTaskAction } = props;
-  const onSubmit = (values, formikBag) => {
 
-    createTaskAction(values);
+  const [deadLine, setDeadline] = useState(null);
+
+  const onDeadlineChange = ({ target }) => {
+    setDeadline(target.value);
+  };
+
+  const onSubmit = (values, formikBag) => {
+    createTaskAction({
+      ...values,
+      deadline: deadLine,
+    });
     formikBag.resetForm();
   };
+
   return (
     <Formik
       onSubmit={onSubmit}
@@ -21,10 +30,24 @@ const TaskForm = props => {
         deadline: null,
       }}
     >
-      <Form className={styles.form}> 
-        <Field name='body' placeholder='Enter new task...' className={styles.input} />
-        <Field name='deadline' type='datetime-local' value={new Date()} className={styles.input} />
-        <button className={styles.submitBtn} type='submit'>Create Task</button>
+      <Form className={styles.form}>
+        <Field
+          name='body'
+          placeholder='Enter new task...'
+          className={styles.input}
+        />
+        <span className={styles.label}>Set deadline:</span>
+        <Field
+          name='deadline'
+          type='datetime-local'
+          value={deadLine}
+          onChange={onDeadlineChange}
+          className={styles.input}
+        />
+
+        <button className={styles.submitBtn} type='submit'>
+          Create Task
+        </button>
       </Form>
     </Formik>
   );
